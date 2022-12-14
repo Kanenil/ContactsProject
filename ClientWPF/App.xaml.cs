233d4fb4b52fec8d8d0ed2cd19b9e110064360a6
Burd_rw81 <Burd_rw81@student.itstep.org>
2022-12-14 12:04:13 +0200

    Basic changes MVVM and add App.config

--- ClientWPF/App.xaml.cs
+++ ClientWPF/App.xaml.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿using BusinnesLogicLayer.Infrastructure;
+using BusinnesLogicLayer.Interfaces;
+using BusinnesLogicLayer.ModelsDTO;
+using BusinnesLogicLayer.Services;
+using ClientWPF.ViewModels;
+using ClientWPF.Views;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +20,28 @@ namespace ClientWPF
     /// </summary>
     public partial class App : Application
     {
+        private IServiceProvider serviceProvider;
+        private readonly string connectionString;
+        public App()
+        {
+            var service = new ServiceCollection();
+            connectionString = ConfigurationManager.ConnectionStrings["ContactDB"].ConnectionString;
+            ConfigureServices(service);
+            serviceProvider = service.BuildServiceProvider();
+
+        }
+        private void ConfigureServices(ServiceCollection collection)
+        {
+            collection.AddSingleton(typeof(IService<ContactDTO>), typeof(ContactService));
+            collection.AddSingleton(typeof(ContactViewModel));
+            collection.AddSingleton(typeof(MainWindow));
+            ConfigurationBLL.ConfigureServices(collection, connectionString);
+            ConfigurationBLL.AddDependecy(collection);
+        }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var mainWindow = (Window)serviceProvider.GetService<MainWindow>();
+            mainWindow.Show();
+        }
     }
 }
---
+++ ClientWPF/ViewModels/ContactViewModel.cs
@@ -0,0 +1,32 @@
+﻿using BusinnesLogicLayer.Interfaces;
+using BusinnesLogicLayer.ModelsDTO;
+using ClientWPF.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace ClientWPF.ViewModels
+{
+    public class ContactViewModel : ViewModel
+    {
+        public readonly IService<ContactDTO> _service;
+        public ObservableCollection<ContactDTO> Contacts { get; set; }
+        private List<ContactDTO> AllContacts { get; set; }
+        public ContactViewModel(IService<ContactDTO> service)
+        {
+            _service = service;
+            AllContacts = new List<ContactDTO>(service.GetContacts());
+            Contacts = new ObservableCollection<ContactDTO>();
+            CopyContacts();
+        }
+        private void CopyContacts()
+        {
+            Contacts.Clear();
+            foreach (var item in AllContacts)
+            {
+                Contacts.Add(item);
+            }
+        }
+    }
+}
